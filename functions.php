c741640079c530d644db35f07ba6010691da6832
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-03-12 19:30:28 +0200

    Twenty Seventeen Child: functions.php Подключение style.css родительской темы

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@
  * Date: 12.03.17
  * Time: 15:22
  */
+error_log("Load child theme-functions");
 
 add_filter('wp_title', 'filterTitle');
 function filterTitle($title) {
@@ -14,4 +15,10 @@ function filterTitle($title) {
 add_filter('the_content', 'filterTheContent');
 function filterTheContent($content) {
     return $content.' - Twenty Seventeen Child';
+}
+
+add_action('wp_enqueue_scripts', 'my_theme_styles' );
+function my_theme_styles() {
+    wp_enqueue_style('parent-theme-css', get_template_directory_uri() .'/style.css' );
+    wp_enqueue_style('child-theme-css', get_stylesheet_directory_uri() .'/style.css', array('parent-theme-css') );
 }
